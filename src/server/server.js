326edaff10f0135c387e090bd9b4bd0488c62ece
Jayden Thomson <jaydenthomson1999@gmail.com>
2019-08-05 15:00:11 +1000

    add server skeleton

---
+++ src/server/server.js
@@ -0,0 +1,12 @@
+const express = require('express'); // used for routing
+const app = express(); //init express
+const http = require('http').Server(app); //used to provide http functionality
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+let server = http.listen(3000, function() {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log(`Server listening on ${host} port: ${port}`);
+});
