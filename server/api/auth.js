23c3b33fb8b8f6d4a02016b392354a5d1af50ff0
Jayden Thomson <jaydenthomson1999@gmail.com>
2019-08-08 17:15:54 +1000

    add users array

--- server/api/auth.js
+++ server/api/auth.js
@@ -10,6 +10,12 @@ class User {
     }
 }
 
+var users = [
+    User("johndoe", "1/1/2000", '18', 'johndoe@domain.com', 'qwerty', true), 
+    User("janedoe", "19/10/2000", '18', 'janedoe@domain.com', 'password', true), 
+    User("bobhawke", "11/03/1929", '89', 'hawkey@domain.com', 'labor', false)
+]
+
 module.exports = function(req, res) {
     console.log("auth api accessed");
 }
